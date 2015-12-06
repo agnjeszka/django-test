554d3f03313da12ea8351212b5d124d00a5cc76d
Agnieszka P <a.potulska@gmail.com>
2015-12-06 23:55:05 +0100

    staticroot set up-fix

--- kalkulator/settings.py
+++ kalkulator/settings.py
@@ -101,6 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = '/home/rattras/python/django-rattras/kalkulator/static'
+STATIC_ROOT = '/home/rattras/python/django-rattras/static'
 STATIC_URL = '/static/'
 
