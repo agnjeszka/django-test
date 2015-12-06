95112100d95c70e1c416ce55eb018cb1c6676f1a
Agnieszka P <a.potulska@gmail.com>
2015-12-06 23:29:55 +0100

    static files  as root

--- kalkulator/settings.py
+++ kalkulator/settings.py
@@ -100,8 +100,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
+#
+# STATIC_URL = '/static/'
+# STATIC_ROOT = ''
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
